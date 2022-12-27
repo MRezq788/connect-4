f8a14d14ab69f08a6e41da7cbc34c9d3bea311df
Mohamed Rezq <91824618+MRezq788@users.noreply.github.com>
2022-12-27 10:53:19 +0200

    adding undo redo

--- main.c
+++ main.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#define ROWS 7
-#define COLS 9
+#include <time.h>
+#define ROWS 6
+#define COLS 7
+int turn=0;
+char disc[] = "XO";
+char grid[ROWS][COLS];
+int savecol[COLS * ROWS] = {0};
+int top = -1;
 
 typedef struct{
-
-	int num;
 	int moves;
 	char disc;
 	int score;
-	char name[];
-
 }player;
 
 player player1, player2;
-char disc[] = "XO";
 
-
-//score function >> evaluate score after each move
-void score(char grid[ROWS][COLS]){
+//score function >> evaluate score after each move 
+void score(){
 	static turn = 0;
 
 	int i, j;
@@ -28,68 +28,119 @@ void score(char grid[ROWS][COLS]){
 	//check horizontal
 	for(i = ROWS - 1; i >= 0; i--){
 		for(j = 0; j < (COLS - 3); j++){
-			if(grid[i][j] == disc[turn] && grid[i][j + 1] == disc[turn] && grid[i][j + 2] == disc[turn] && grid[i][j + 3] == disc[turn]){
+			if((grid[i][j] == disc[turn] )&&( grid[i][j + 1] == disc[turn] )&&( grid[i][j + 2] == disc[turn] )&&( grid[i][j + 3] == disc[turn])){
 				score += 1;
 			}
-		}
+		}	
 	}
 	//check vertical
-	for(j = 0 ; j < COLS ; j++){
-		for(i = ROWS - 1; i > 2; i--){
-			if(grid[i][j] == disc[turn] && grid[i - 1][j] == disc[turn] && grid[i - 2][j] == disc[turn] && grid[i - 3][j] == disc[turn]){
+	for(i = ROWS - 1; i > 2; i--){
+		for(j = 0 ; j < COLS ; j++){
+			if((grid[i][j] == disc[turn] )&&( grid[i - 1][j] == disc[turn] )&&( grid[i - 2][j] == disc[turn] )&&( grid[i - 3][j] == disc[turn])){
 				score += 1;
 			}
-		}
+		}	
 	}
 	//check diagonal
 	//check bottom left to right
 	for(i = ROWS - 1; i > 2; i--){
 		for(j = 0; j < COLS - 3; j++){
-			if(grid[i][j] == disc[turn] && grid[i - 1][j + 1] == disc[turn] && grid[i - 2][j + 2] == disc[turn] && grid[i - 3][j + 3] == disc[turn]){
+			if((grid[i][j] == disc[turn] )&&( grid[i - 1][j + 1] == disc[turn] )&&( grid[i - 2][j + 2] == disc[turn] )&&( grid[i - 3][j + 3] == disc[turn])){
 				score += 1;
 			}
 		}
 	}
 	//check bottom right to left
-	for(i = ROWS - 1; i > 2; i--){
-		for(j = COLS - 1; j > 2 ; j--){
-			if(grid[i][j] == disc[turn] && grid[i - 1][j - 1] == disc[turn] && grid[i - 2][j - 2] == disc[turn] && grid[i - 3][j - 3] == disc[turn]){
+	for(i = ROWS - 1; i >= 3; i--){
+		for(j = COLS - 1; j >=3 ; j--){
+			if((grid[i][j] == disc[turn] )&&( grid[i - 1][j - 1] == disc[turn] )&&( grid[i - 2][j - 2] == disc[turn] )&&( grid[i - 3][j - 3] == disc[turn])){
 				score += 1;
 			}
 		}
 	}
 	if(turn == 0){
-		player1.score += score;
+		player1.score = score;
 	}else{
-		player2.score += score;
+		player2.score = score;
 	}
 	turn = 1 - turn;
 }
+// add each move's col. to savecol[] to be recorded
+void push(int col/*users col*/){
+	top++;
+	savecol[ top ] = col;
+	savecol[ top + 1 ] = 0;
+	
+}
+
+
+//check grid full or not 
+int isfull(){
+	int j;
+	for(j = 0; j < COLS; j++ ){
+		if(grid[0][j] != '\0'){
+		return 0;
+		}
+	}
+	return 1;
+	
+}
+
+
+void undo(/*int col users col*/ int lastrow[COLS]){
+    if(top != - 1 || savecol[top] != 0){	
+		grid[ lastrow[ savecol[ top ] + 1 ] ][ savecol[ top ] - 1 ] = '\0';
+		top--;
+//		turn = 1 - turn;  
+	}
+	score();
+}
+
+void redo( int lastrow[COLS]){
+	top++;
+	if(savecol[top] != 0){
+		grid[ lastrow[ savecol[ top ] - 1 ] + 1 ][ savecol[ top ] - 1] = disc[turn];
+	}
+	score();
+}
+
+// print ellapsed time after each move
+ void timee(long t){
+    long time_now=time(NULL);
+    long time_passed = time_now - t;
+    long sec = time_passed % 60;
+    long min = time_passed / 60;
+    printf("\033[0;33m");
+    if(sec<10&&min<10){
+      printf("\nTimer 0%ld:0%ld\n",min,sec);
+    }
+    else if(sec>10&&min<10){
+      printf("\nTimer0%ld:%ld\n",min,sec);
+    }
+    else if(sec<10&&min>10){
+      printf("\nTimer%ld:0%ld\n",min,sec);
+    }
+    else{
+      printf("\nTimer%ld:%ld\n",min,sec);
+    }
+}
+
 //last_row function >> returns index of last empty row in specific columns
 int last_row(int col/*users col*/, int lastrow[col]){
 	if(lastrow[col - 1] == 0){
 		lastrow[col - 1 ] = ROWS - 1;
-		return lastrow[col - 1];
+		return lastrow[col - 1]; 
 	}else{
 		lastrow[col - 1] = lastrow[col - 1] - 1;
 		return lastrow[col - 1];
 	}
 }
 //print_grid function
-void print_grid(char grid[ROWS][COLS])
-{
+void print_grid(long t){
     int i ,j;
-
-    /*system("color f1");*/ //TO COLOR THE BOARD
-    for( int ii = 1;ii <= COLS; ii++){
-          if(log10(ii)<1){
-            printf("  %d ",ii);
-          }
-          else{
-            printf(" %d",ii);
-          }
-    }
-        printf("\n");
+    printf("\e[1;H\e[2J");    //clear terminal
+    timee(t);
+    printf("\033[0;32m");                    //TO COLOR THE BOARD green
     for ( i = 0; i < ROWS; i++)
     {
         for ( j = 0; j < COLS; j++)
@@ -99,13 +150,23 @@ void print_grid(char grid[ROWS][COLS])
         printf("+\n");
         for ( j = 0; j < COLS; j++)
         {
-            if(grid[i][j] != 'X' && grid[i][j] != 'O'  ){
+            if((grid[i][j] != disc[0] )&&( grid[i][j] != disc[1]  )){
 				printf("|   ");
+				}else{
+					printf("| ");
+					
+					if(grid[i][j] == disc[0] ){
+						printf("\033[0;35m");
+						printf("%c ", grid[i][j]);
+					}else{
+						printf("\033[0;36m");
+						printf("%c ", grid[i][j]);
+					}
+					printf("\033[0;32m");                    //TO COLOR THE BOARD green
+					
+					
 				}
-				else{
-					printf("| %c ", grid[i][j]);
-				}
-
+			
         }
         printf("|\n");
     }
@@ -114,95 +175,90 @@ void print_grid(char grid[ROWS][COLS])
             printf("+---");
         }
         printf("+\n");
-}
-void move(char grid[ROWS][COLS]){
-
-    int static turn=0;
-    int lastr[COLS],col;
-    player1.moves=0;
-    player1.score=0;
-    player2.moves=0;
-    player2.score=0;
-
-    memset(lastr, 0 ,sizeof lastr);
-    int n=ROWS*COLS;
-
-    while(n!=0){
-
-        printf("\nchoose column\n");
-        scanf("%d",&col);
-
-        while(check_move(col)==0){               //checking limits
-            printf("INVALID INPUT\n");
-            scanf("%d",&col);
-        }
-
-        int r = last_row(col,lastr);
-       /* while( grid[r][col-1]=='X'||grid[r][col-1]=='O'){
-            printf("Choose another col\n");
-            scanf("%d",&col);
-        }*/
-
-
-        grid[r][col-1]=disc[turn];
-        print_grid(grid);
-
-        if(disc[turn]=='X'){     //count moves
-            player1.moves++;
-        }
-        if(disc[turn]=='O'){
-            player2.moves++;
-        }
-
-        turn = 1- turn;     //taking turns
-        n--;
-        score(grid);
+        printf("[u]Undo [r]Redo [e]Exit [s]Save \n\n");
+        printf("\033[0;37m");     // to color white 
         printf("P1 score : %d  P2 score: %d\n",player1.score,player2.score);
-        printf("P1 moves : %d  P2 moves: %d\n",player1.moves,player2.moves);
-    }
+        printf("P1 moves : %d  P2 moves: %d\n\n",player1.moves,player2.moves);
 }
-int check_move(n){
-    int count=0;                           //check if the column chosen is valid
-    for(int i=1;i<=COLS;i++){
-        if(n==i){
-            count++;
-        }
-    }
-    return count;
-}
-void start(char grid[ROWS][COLS]){
-    char select;
-    printf("MAIN MENU\n");
-    printf("1:Start a new game.\n2:load game.\n3:Top players.\n4:Quit.\n");
-    scanf("%c",&select);
-    switch(select){
-        case'1':print_grid(grid);
-                break;
-    }
-}
-void check_winner(){
-    if(player1.score>player2.score){
-        printf("\np1 is the winner\nscore=%d",player1.score);
-    }
-    else if(player2,score>player1.score){
-        printf("\np2 is the winner\n score=%d",player2.score);
-    }
-    else{
-        printf("\nIt's a tie");
-    }
-}
-int main() {
-    printf("CONNECT FOUR\n\n");
 
-	int lastrow[COLS],col;
-	memset(lastrow , 0 ,sizeof lastrow );
+// moves count moves of each player
+void moves(int turn){
+	 if(disc[turn]==disc[0]){     //count moves
+            player1.moves++;
+            score();
+        }else if(disc[turn]== disc[1]){
+            	player2.moves++;
+        		score();	
+        }	
+}
 
-	char grid[ROWS][COLS];
-    start(grid);
-	player1.disc = 'X';
-	player2.disc = 'O';
 
-	move(grid);
-	check_winner();
-   return 0;
+// fill_grid 
+void fill_grid(int col/*users col*/, int lastrow[COLS]){
+    grid[last_row(col,lastrow)][col-1] = disc[turn];
+	moves(turn); 
+	}
+
+void steering(int lastrow[COLS]){
+
+	if(turn == 0){
+		printf("\033[0;35m");
+	}else{
+		printf("\033[0;36m");
+	}	
+	printf("Player %d , your turn :\n", turn + 1);
+	int wheel;
+	scanf("%d", &wheel);
+	while( wheel > -5){
+	
+	 
+		if(wheel == -1){              // 85  U  117  u
+			undo(lastrow);
+		}else if(wheel == -2 ){        //82  R   114  r
+			redo(lastrow);				
+		}else if( wheel > 0 && wheel <= COLS){
+				if(grid[0][wheel - 1] == '\0'){
+					fill_grid(wheel, lastrow);
+					push(wheel);
+					turn = 1 - turn; 
+					break;
+				}else{
+			 		printf("\033[0;31m");
+			 		printf("THIS COLUMN IS FULL, PLEASE CHOOSE ANOTHER ONE:\n");
+			 		scanf("%d", &wheel);	
+				}
+		}else{
+			 printf("\033[0;31m");
+			 printf("PLEASE ENTER A VALID INPUT :\n");
+			 scanf("%d", &wheel);
+			
+		}
+		//else{
+//			computer();
+//		}
+
+//else if(wheel == -3 ){         //69  E   101  e
+	//		exit();
+	//	}else if(wheel == -4 ){        //83  S    115  s
+	//		save();
+	//	}else
+    //else if(wheel == -3 ){         //69  E   101  e
+	//		exit();
+	//	}else if(wheel == -4 ){        //83  S    115  s
+	//		save();
+	//	}else
+	}
+}
+
+int main(){
+	long start=time(NULL);
+	int lastrow[COLS];
+	memset(lastrow, 0 ,sizeof lastrow);
+	
+	while( isfull() ){
+		print_grid(start);
+		steering(lastrow);
+    }
+	
+	return 0;
 }
